270fe79f25966df9a0bba11a858cc1bd22b26ab1
NadaAlinour <lime.x.na@gmail.com>
2024-02-01 10:06:15 +0200

    Implement optimized bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -10,6 +10,28 @@ def bubble_sort(arr)
   arr
 end
 
+def optimized_bubble_sort(arr)
+  is_sorted = false
+  while !is_sorted
+    is_sorted = true
+    for i in 0..arr.length - 2
+      for j in i + 1 .. arr.length - 1
+        if (arr[i] > arr[j])
+          # swap
+          arr[i], arr[j] = arr[j], arr[i]
+          is_sorted = false
+        end
+      end
 
-sorted_result = bubble_sort([4,3,78,2,0,2])
+    end
+  end
+    
+    arr
+
+end
+
+# sorted_result = bubble_sort([4,3,78,2,0,2])
+# p sorted_result
+
+sorted_result = optimized_bubble_sort([4,3,78,2,0,2])
 p sorted_result
